6c9f9a3e946f75c69e9d0e8b8d5229c177389c81
rodrigo1112 <rodrigo.girardi99@gmail.com>
2023-08-14 12:54:49 -0300

    Updates

--- HomeBanking.js
+++ HomeBanking.js
@@ -1,132 +1,137 @@
-function createUser() {
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const email = document.getElementById("email").value;
+// Array para almacenar usuarios y sus cuentas
+const usuarios = [];
 
-    if (firstName.trim() === "" || lastName.trim() === "" || email.trim() === "") {
-        return;
-    }   
-
-    const user = {
-        firstName,
-        lastName,
-        email,
-        CA: 1000, 
-        CC: 0,    
-        CAUSD: 0, 
-        CUSD: 0,  
-        transactions: []
-    };
-
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    users.push(user);
-    localStorage.setItem("users", JSON.stringify(users));
-
-    displayUsers();
-    document.getElementById("firstName").value = "";
-    document.getElementById("lastName").value = "";
-    document.getElementById("email").value = "";
+// Función para crear una nueva cuenta
+function crearCuenta(tipo, saldo) {
+  return {
+    tipo,
+    saldo
+  };
 }
 
-function deleteUser(index) {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    users.splice(index, 1);
-    localStorage.setItem("users", JSON.stringify(users));
-    displayUsers();
+// Función para crear un nuevo usuario
+function crearUsuario(nombre, apellido, email) {
+  const usuario = {
+    nombre,
+    apellido,
+    email,
+    cuentas: []
+  };
+
+  usuario.cuentas.push(crearCuenta('Caja de ahorro en pesos', 1000)); // Agregar cuenta con saldo inicial de $1000
+
+  usuarios.push(usuario); // Agregar usuario al array
+
+  return usuario;
 }
 
-function handleTransaction(type, index) {
-    const amount = parseFloat(prompt(`Ingrese el monto a ${type === "withdraw" ? "retirar" : "depositar"}:`));
-    if (isNaN(amount) || amount <= 0) {
-        return;
+// Función para depositar dinero en una cuenta
+function depositar(usuario, cuentaIndex, monto) {
+  usuario.cuentas[cuentaIndex].saldo += monto;
+}
+
+// Función para retirar dinero de una cuenta
+function retirar(usuario, cuentaIndex, monto) {
+  const cuenta = usuario.cuentas[cuentaIndex];
+  if (cuenta.tipo === 'Caja de ahorro en pesos' && cuenta.saldo - monto < 0) {
+    return false; // No se permite saldo negativo en caja de ahorro
+  }
+
+  if (cuenta.tipo === 'Cuenta corriente en pesos' && cuenta.saldo - monto < -1000) {
+    return false; // No se permite saldo negativo mayor a -1000 en cuenta corriente
+  }
+
+  cuenta.saldo -= monto;
+  return true;
+}
+
+// Función para realizar una transferencia entre cuentas
+function transferir(origenUsuario, origenCuentaIndex, destinoUsuario, destinoCuentaIndex, monto) {
+  const origenCuenta = origenUsuario.cuentas[origenCuentaIndex];
+  const destinoCuenta = destinoUsuario.cuentas[destinoCuentaIndex];
+
+  if (origenCuenta.saldo - monto < 0) {
+    return false;
+  }
+
+  origenCuenta.saldo -= monto;
+  destinoCuenta.saldo += monto;
+  return true;
+}
+
+// Función para eliminar un usuario
+function eliminarUsuario(usuarioIndex) {
+  usuarios.splice(usuarioIndex, 1);
+}
+
+// Función para agregar un usuario a la lista de cuentas en el HTML
+function agregarUsuarioALista(usuario) {
+  const listaCuentas = document.getElementById('listaCuentas');
+  const nuevoUsuarioItem = document.createElement('li');
+  nuevoUsuarioItem.textContent = `${usuario.nombre} ${usuario.apellido} - ${usuario.email}`;
+  listaCuentas.appendChild(nuevoUsuarioItem);
+}
+
+// Función para actualizar la interfaz de usuario con las cuentas de un usuario
+function mostrarCuentas(usuario) {
+  const listaUsuarios = document.getElementById('listaUsuarios');
+  const nuevoUsuarioItem = document.createElement('li');
+  nuevoUsuarioItem.textContent = `${usuario.nombre} ${usuario.apellido} - ${usuario.email}`;
+
+  const depositarBtn = document.createElement('button');
+  depositarBtn.textContent = 'Depositar';
+  depositarBtn.addEventListener('click', () => {
+    const monto = parseFloat(prompt('Ingrese el monto a depositar:'));
+    if (!isNaN(monto)) {
+      const cuentaIndex = 0; // Cambia esto para seleccionar la cuenta deseada
+      depositar(usuario, cuentaIndex, monto);
+      actualizarSaldoCuenta(usuario, cuentaIndex);
     }
+  });
 
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    const user = users[index];
-
-    const account = prompt("Ingrese la cuenta (CA, CUSD, CC, CAUSD):").toUpperCase();
-    if (!user.hasOwnProperty(account)) {
-        return;
+  const retirarBtn = document.createElement('button');
+  retirarBtn.textContent = 'Retirar';
+  retirarBtn.addEventListener('click', () => {
+    const monto = parseFloat(prompt('Ingrese el monto a retirar:'));
+    if (!isNaN(monto)) {
+      const cuentaIndex = 0; // Cambia esto para seleccionar la cuenta deseada
+      if (retirar(usuario, cuentaIndex, monto)) {
+        actualizarSaldoCuenta(usuario, cuentaIndex);
+      } else {
+        alert('No se puede retirar el monto especificado.');
+      }
     }
+  });
 
-    if (type === "deposit") {
-        user[account] += amount;
-    } else if (type === "withdraw") {
-        if (account === "CA" && user[account] - amount < -1000) {
-            return;
-        }
-        if (account === "CUSD" && user[account] - amount < 0) {
-            return;
-        }
-        user[account] -= amount;
-    } else if (type === "transfer") {
-        const targetAccount = prompt("Ingrese la cuenta de destino (CA, CUSD, CC, CAUSD):").toUpperCase();
-        if (!user.hasOwnProperty(targetAccount)) {
-            return;
-        }
-        if (user[account] - amount < 0) {
-            return;
-        }
-        user[account] -= amount;
-        user[targetAccount] += amount;
+  const transferirBtn = document.createElement('button');
+  transferirBtn.textContent = 'Transferir';
+  transferirBtn.addEventListener('click', () => {
+    const monto = parseFloat(prompt('Ingrese el monto a transferir:'));
+    if (!isNaN(monto)) {
+      const origenCuentaIndex = 0; // Cambia esto para seleccionar la cuenta de origen
+      const destinoCuentaIndex = 1; // Cambia esto para seleccionar la cuenta de destino
+      transferir(usuario, origenCuentaIndex, usuario, destinoCuentaIndex, monto);
+      actualizarSaldoCuenta(usuario, origenCuentaIndex);
+      actualizarSaldoCuenta(usuario, destinoCuentaIndex);
     }
-    
-    user.transactions.push(`Transacción: ${getTransactionType(type)} de ${amount} desde ${getAccountName(account)} a ${getAccountName(targetAccount)}. Nuevo Saldo: ${user[account]}`);
-    
-    
-    user.transactions.push(`Transaccion: ${type === "deposit" ? "Depósito" : "Retiro"} de ${amount} en ${getAccountName(account)}. Nuevo Saldo: ${user[account]}`);
-    localStorage.setItem("users", JSON.stringify(users));
-    displayUsers();
+  });
+
+  // Agregar los botones a la lista de acciones del usuario
+  nuevoUsuarioItem.appendChild(depositarBtn);
+  nuevoUsuarioItem.appendChild(retirarBtn);
+  nuevoUsuarioItem.appendChild(transferirBtn);
+
+  listaUsuarios.appendChild(nuevoUsuarioItem);
 }
 
-function getAccountName(account) {
-    const accountNames = {
-        CA: "Caja de Ahorro (PESOS)",
-        CC: "Cuenta Corriente (PESOS)",
-        CAUSD: "Caja de Ahorro (USD)",
-        CUSD: "Cuenta Corriente (USD)"
-    };
-    return accountNames[account] || "Cuenta Desconocida";
-}
+// Función para actualizar el saldo de una cuenta en la interf
 
-function displayUsers() {
-    const userList = document.getElementById("userList");
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    userList.innerHTML = "";
+// Ejemplo de uso
+const usuario1 = crearUsuario('Juan', 'Perez', 'juan@example.com');
+const usuario2 = crearUsuario('Maria', 'Lopez', 'maria@example.com');
 
-    users.forEach((user, index) => {
-        const li = document.createElement("li");
-        li.textContent = `${user.firstName} ${user.lastName} - Email: ${user.email}`;
+// Agregar usuarios a la lista de cuentas en el HTML
+agregarUsuarioALista(usuario1);
+agregarUsuarioALista(usuario2);
 
-        if (user.transactions && user.transactions.length > 0) {
-            const ul = document.createElement("ul");
-            user.transactions.forEach(transaction => {
-                const transactionLi = document.createElement("li");
-                transactionLi.textContent = transaction;
-                ul.appendChild(transactionLi);
-            });
-            li.appendChild(ul);
-        }
 
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Eliminar";
-        deleteButton.addEventListener("click", () => deleteUser(index));
-        const depositButton = document.createElement("button");
-        depositButton.textContent = "Depositar";
-        depositButton.addEventListener("click", () => handleTransaction("deposit", index));
-        const withdrawButton = document.createElement("button");
-        withdrawButton.textContent = "Retirar";
-        withdrawButton.addEventListener("click", () => handleTransaction("withdraw", index));
-        li.appendChild(deleteButton);
-        li.appendChild(depositButton);
-        li.appendChild(withdrawButton);
-        userList.appendChild(li);
-        const transferButton = document.createElement("button");
-transferButton.textContent = "Transferir";
-transferButton.addEventListener("click", () => handleTransaction("transfer", index));
-li.appendChild(transferButton);
-
-    });
-}
-
-displayUsers();
