f43673a3ee716ec188ced53279efd18f24f32734
rodrigo1112 <rodrigo.girardi99@gmail.com>
2023-08-18 13:31:19 -0300

    elimine el html, ahora console.log

--- HomeBanking.js
+++ HomeBanking.js
@@ -1,137 +1,160 @@
-// Array para almacenar usuarios y sus cuentas
-const usuarios = [];
+const readline = require('readline');
 
-// Función para crear una nueva cuenta
-function crearCuenta(tipo, saldo) {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const TipoCuenta = {
+  CAJA_DE_AHORRO: 'Caja de ahorro en pesos',
+  CUENTA_CORRIENTE: 'Cuenta corriente en pesos'
+};
+
+function generarID() {
+  return Math.floor(10000 + Math.random() * 90000); // Genera un número aleatorio de 5 dígitos
+}
+
+function crearCuenta(tipo, saldo = 0) {
   return {
     tipo,
-    saldo
+    saldo,
+    id: generarID(),
+    movimientos: []
   };
 }
 
-// Función para crear un nuevo usuario
 function crearUsuario(nombre, apellido, email) {
   const usuario = {
-    nombre,
-    apellido,
-    email,
+    nombre: nombre,
+    apellido: apellido,
+    email: email,
     cuentas: []
   };
 
-  usuario.cuentas.push(crearCuenta('Caja de ahorro en pesos', 1000)); // Agregar cuenta con saldo inicial de $1000
+  const nuevaCuenta = crearCuenta(TipoCuenta.CAJA_DE_AHORRO, 1000);
+  usuario.cuentas.push(nuevaCuenta);
 
-  usuarios.push(usuario); // Agregar usuario al array
+  usuarios.push(usuario);
 
   return usuario;
 }
 
-// Función para depositar dinero en una cuenta
-function depositar(usuario, cuentaIndex, monto) {
-  usuario.cuentas[cuentaIndex].saldo += monto;
+// Resto de las funciones (depositar, retirar, transferir, comprarDolares, venderDolares, etc.)
+
+const usuarios = [];
+
+function mostrarMenu() {
+  console.log('--- Menú ---');
+  console.log('1. Transferencias');
+  console.log('2. Ingresar dinero (depósito)');
+  console.log('3. Retiro de dinero (extracción)');
+  console.log('4. Compra de dólares');
+  console.log('5. Venta de dólares');
+  console.log('6. Ver cuentas creadas');
+  console.log('7. Ver historial de movimientos');
+  console.log('8. Crear cuenta');
+  console.log('0. Salir');
 }
 
-// Función para retirar dinero de una cuenta
-function retirar(usuario, cuentaIndex, monto) {
-  const cuenta = usuario.cuentas[cuentaIndex];
-  if (cuenta.tipo === 'Caja de ahorro en pesos' && cuenta.saldo - monto < 0) {
-    return false; // No se permite saldo negativo en caja de ahorro
-  }
-
-  if (cuenta.tipo === 'Cuenta corriente en pesos' && cuenta.saldo - monto < -1000) {
-    return false; // No se permite saldo negativo mayor a -1000 en cuenta corriente
-  }
-
-  cuenta.saldo -= monto;
-  return true;
-}
-
-// Función para realizar una transferencia entre cuentas
-function transferir(origenUsuario, origenCuentaIndex, destinoUsuario, destinoCuentaIndex, monto) {
-  const origenCuenta = origenUsuario.cuentas[origenCuentaIndex];
-  const destinoCuenta = destinoUsuario.cuentas[destinoCuentaIndex];
-
-  if (origenCuenta.saldo - monto < 0) {
-    return false;
-  }
-
-  origenCuenta.saldo -= monto;
-  destinoCuenta.saldo += monto;
-  return true;
-}
-
-// Función para eliminar un usuario
-function eliminarUsuario(usuarioIndex) {
-  usuarios.splice(usuarioIndex, 1);
-}
-
-// Función para agregar un usuario a la lista de cuentas en el HTML
-function agregarUsuarioALista(usuario) {
-  const listaCuentas = document.getElementById('listaCuentas');
-  const nuevoUsuarioItem = document.createElement('li');
-  nuevoUsuarioItem.textContent = `${usuario.nombre} ${usuario.apellido} - ${usuario.email}`;
-  listaCuentas.appendChild(nuevoUsuarioItem);
-}
-
-// Función para actualizar la interfaz de usuario con las cuentas de un usuario
-function mostrarCuentas(usuario) {
-  const listaUsuarios = document.getElementById('listaUsuarios');
-  const nuevoUsuarioItem = document.createElement('li');
-  nuevoUsuarioItem.textContent = `${usuario.nombre} ${usuario.apellido} - ${usuario.email}`;
-
-  const depositarBtn = document.createElement('button');
-  depositarBtn.textContent = 'Depositar';
-  depositarBtn.addEventListener('click', () => {
-    const monto = parseFloat(prompt('Ingrese el monto a depositar:'));
-    if (!isNaN(monto)) {
-      const cuentaIndex = 0; // Cambia esto para seleccionar la cuenta deseada
-      depositar(usuario, cuentaIndex, monto);
-      actualizarSaldoCuenta(usuario, cuentaIndex);
-    }
+function verCuentasCreadas() {
+  usuarios.forEach((usuario, index) => {
+    console.log(`Usuario ${index + 1}: ${usuario.nombre} ${usuario.apellido}`);
+    usuario.cuentas.forEach((cuenta, cuentaIndex) => {
+      console.log(`  Cuenta ${cuentaIndex + 1}: Tipo: ${cuenta.tipo}, Saldo: $${cuenta.saldo}, ID: ${cuenta.id}`);
+    });
   });
-
-  const retirarBtn = document.createElement('button');
-  retirarBtn.textContent = 'Retirar';
-  retirarBtn.addEventListener('click', () => {
-    const monto = parseFloat(prompt('Ingrese el monto a retirar:'));
-    if (!isNaN(monto)) {
-      const cuentaIndex = 0; // Cambia esto para seleccionar la cuenta deseada
-      if (retirar(usuario, cuentaIndex, monto)) {
-        actualizarSaldoCuenta(usuario, cuentaIndex);
-      } else {
-        alert('No se puede retirar el monto especificado.');
-      }
-    }
-  });
-
-  const transferirBtn = document.createElement('button');
-  transferirBtn.textContent = 'Transferir';
-  transferirBtn.addEventListener('click', () => {
-    const monto = parseFloat(prompt('Ingrese el monto a transferir:'));
-    if (!isNaN(monto)) {
-      const origenCuentaIndex = 0; // Cambia esto para seleccionar la cuenta de origen
-      const destinoCuentaIndex = 1; // Cambia esto para seleccionar la cuenta de destino
-      transferir(usuario, origenCuentaIndex, usuario, destinoCuentaIndex, monto);
-      actualizarSaldoCuenta(usuario, origenCuentaIndex);
-      actualizarSaldoCuenta(usuario, destinoCuentaIndex);
-    }
-  });
-
-  // Agregar los botones a la lista de acciones del usuario
-  nuevoUsuarioItem.appendChild(depositarBtn);
-  nuevoUsuarioItem.appendChild(retirarBtn);
-  nuevoUsuarioItem.appendChild(transferirBtn);
-
-  listaUsuarios.appendChild(nuevoUsuarioItem);
 }
 
-// Función para actualizar el saldo de una cuenta en la interf
+function verHistorialMovimientos() {
+  usuarios.forEach((usuario, index) => {
+    console.log(`Historial de movimientos para ${usuario.nombre} ${usuario.apellido}:`);
+    usuario.cuentas.forEach((cuenta, cuentaIndex) => {
+      console.log(`  Cuenta ${cuentaIndex + 1} (${cuenta.tipo}, ID: ${cuenta.id}):`);
+      cuenta.movimientos.forEach((movimiento, movimientoIndex) => {
+        console.log(`    ${movimiento}`);
+      });
+    });
+  });
+}
 
-// Ejemplo de uso
-const usuario1 = crearUsuario('Juan', 'Perez', 'juan@example.com');
-const usuario2 = crearUsuario('Maria', 'Lopez', 'maria@example.com');
+async function crearNuevaCuenta() {
+  const nombre = await preguntaAsync('Ingrese el nombre del nuevo usuario: ');
+  const apellido = await preguntaAsync('Ingrese el apellido del nuevo usuario: ');
+  const email = await preguntaAsync('Ingrese el email del nuevo usuario: ');
 
-// Agregar usuarios a la lista de cuentas en el HTML
-agregarUsuarioALista(usuario1);
-agregarUsuarioALista(usuario2);
+  const nuevoUsuario = crearUsuario(nombre, apellido, email);
+  console.log(`Se ha creado una nueva cuenta para ${nuevoUsuario.nombre} ${nuevoUsuario.apellido}`);
+}
+
+function preguntaAsync(pregunta) {
+  return new Promise((resolve) => {
+    rl.question(pregunta, (respuesta) => {
+      resolve(respuesta);
+    });
+  });
+}
+
+async function ejecutarBanco() {
+  mostrarMenu();
+  await solicitarOpcion();
+}
+
+async function solicitarOpcion() {
+  const input = await preguntaAsync('Ingrese el número de opción: ');
+  const opcion = parseInt(input);
+
+  switch (opcion) {
+    case 1:
+      // Realizar transferencias
+      // ...
+      await solicitarOpcion();
+      break;
+    case 2:
+      // Realizar depósito
+      // ...
+      await solicitarOpcion();
+      break;
+    case 3:
+      // Realizar retiro
+      // ...
+      await solicitarOpcion();
+      break;
+    case 4:
+      // Realizar compra de dólares
+      // ...
+      await solicitarOpcion();
+      break;
+    case 5:
+      // Realizar venta de dólares
+      // ...
+      await solicitarOpcion();
+      break;
+    case 6:
+      // Ver cuentas creadas
+      verCuentasCreadas();
+      await solicitarOpcion();
+      break;
+    case 7:
+      // Ver historial de movimientos
+      verHistorialMovimientos();
+      await solicitarOpcion();
+      break;
+    case 8:
+      // Crear cuenta
+      await crearNuevaCuenta();
+      await solicitarOpcion();
+      break;
+    case 0:
+      console.log('Saliendo del banco. ¡Hasta luego!');
+      rl.close();
+      break;
+    default:
+      console.log('Opción inválida. Por favor, ingrese una opción válida.');
+      await solicitarOpcion();
+      break;
+  }
+}
+
+ejecutarBanco();
 
 
